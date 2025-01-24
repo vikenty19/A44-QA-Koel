6c782d31be86ea662efd79430df34dfa419d3bdc
vikenty19 <vikenty.plakhov@testpro.io>
2025-01-24 15:57:11 -0500

    login

--- src/test/java/SmartPlayListTest.java
+++ src/test/java/SmartPlayListTest.java
@@ -1,9 +1,16 @@
+import POM.LoginPage;
+import POM.PlayListPage;
 import org.testng.annotations.Test;
 
 public class SmartPlayListTest extends BaseTest {
 
     @Test
-    public void createPlistWithName(){
+    public void createPlistWithName() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        PlayListPage playListPage = new PlayListPage(driver);
+        loginPage.login(myEmail,myLogin);
+        playListPage.plusBtnClick();
+        Thread.sleep(5000);
 
 
     }
