dfaca301f25463846c0f74ed570125793af1f285
vikenty19 <vikenty.plakhov@testpro.io>
2025-02-11 17:53:53 -0500

    pickBrowser method added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,5 @@
 import com.github.javafaker.Faker;
+import com.github.javafaker.Options;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.OperaDriverManager;
 import org.openqa.selenium.By;
@@ -7,6 +8,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,6 +20,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Locale;
 
@@ -26,23 +32,47 @@ public class BaseTest {
     public static String myEmail = "galy.o@testpro.io";
     public static WebDriverWait wait = null;
 
-    @BeforeSuite
-    static void setupDriver() {
+//    @BeforeSuite
+  /*  static void setupDriver() {
         WebDriverManager.chromedriver().clearDriverCache().setup();
         WebDriverManager.chromedriver().setup();
+    }*/
+    public static WebDriver pickBrowser(String browserName) throws MalformedURLException {
+        WebDriver driver;
+        DesiredCapabilities dc = new DesiredCapabilities();
+        if (browserName.equalsIgnoreCase("chrome")){
+            dc.setBrowserName("chrome");
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            dc.setBrowserName("firefox");
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--disable-notifications");
+
+        } else if (browserName.equalsIgnoreCase("edge")) {
+            dc.setBrowserName("MicrosoftEdge");
+        } else if (browserName.equalsIgnoreCase("opera")) {
+            dc.setBrowserName("opera");
+        } else if (browserName.equalsIgnoreCase("ie")) {
+            dc.setBrowserName("internet explorer");
+
+        }
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"),dc);
+        return driver;
     }
 
+
+
     @BeforeMethod
-    public void setUpBrowser() {
-        ChromeOptions options = new ChromeOptions();
+    public void setUpBrowser() throws MalformedURLException {
+    /*    ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--disable-notifications");
         options.addArguments("--start-maximized");
         // System.setProperty("webdriver.chromedriver","C:\\Users\\Acer\\Downloads\\chrome-win64 (1).zip\\chrome-win64\\");
 
-        driver = new ChromeDriver(options);
-
-
+        driver = new ChromeDriver(options);*/
+        driver=pickBrowser("firefox");
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--disable-notifications");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         openUrl(url);
