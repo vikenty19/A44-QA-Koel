454b7631fc732fc2553a36acb9e46bb6862a2ad2
vikenty19 <vikenty.plakhov@testpro.io>
2025-03-07 15:57:00 -0500

    Login success added
    FindBy replaced!

--- src/test/java/POM/MyAccountPage.java
+++ src/test/java/POM/MyAccountPage.java
@@ -1,15 +1,18 @@
 package POM;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static StepDefinitions.TutorialsLoginOnly.driver;
+import static StepDefinitions.TutorialsLoginOnly.wait;
 
 public class MyAccountPage {
     public MyAccountPage(){
         PageFactory.initElements(driver,this);
     }
-    @FindBy(css = ".breadcrumb>li:nth-child(2)>a")
-    public static WebElement accountBreadCrumb;
+    public static By account = By.cssSelector(".breadcrumb>li:nth-child(2)>a");
+    public static WebElement accountBreadCrumb = wait.until(ExpectedConditions.visibilityOfElementLocated(account));
 }
--- src/test/java/POM/TutorialLoginPage.java
+++ src/test/java/POM/TutorialLoginPage.java
@@ -1,20 +1,23 @@
 package POM;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static StepDefinitions.TutorialsLoginOnly.driver;
+import static StepDefinitions.TutorialsLoginOnly.wait;
 
 public class TutorialLoginPage {
     public TutorialLoginPage(){
         PageFactory.initElements(driver,this);
     }
-    @FindBy(id = "input-email")
-    public static WebElement emailField;
-    @FindBy(id="input-password")
-    public static WebElement passwordField;
-    @FindBy(css = "input[type ='submit']")
-    public static WebElement loginSubmitBtn;
+    public static By inputEmail = By.id("input-email");
+    public static WebElement emailField = wait.until(ExpectedConditions.elementToBeClickable(inputEmail));
+    public static By inputPassword = By.id("input-password");
+    public static WebElement passwordField = wait.until(ExpectedConditions.elementToBeClickable(inputPassword));
+    public static By submitLogin = By.cssSelector("input[type ='submit']");
+    public static WebElement loginSubmitBtn =wait.until(ExpectedConditions.elementToBeClickable(submitLogin));
 
 }
